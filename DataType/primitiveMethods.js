be89665d1f4e4effde686b88631441a69bc474dc
Vyshnav <svyshnavraj@gmail.com>
2020-02-21 15:51:24 +0530

    primitive methods introduction

---
+++ DataType/primitiveMethods.js
@@ -0,0 +1,9 @@
+let str = "hello, world"
+
+console.log(str.toUpperCase()) // wrapper created, function executed, output returned and wrapper destroyed
+
+let num = 328.23784
+console.log(num.toFixed(2))
+
+console.log("hi".toUpperCase()) // Also works - no need to store in a variable
+console.log(763523.237826.toFixed(2))
