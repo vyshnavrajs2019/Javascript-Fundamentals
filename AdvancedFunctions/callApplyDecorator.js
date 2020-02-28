263912d540c1937634f15fd1c266ed21c0a64b7c
vyshnav-pt3550 <vyshnav-pt3550@tsi.zohocorpin.com>
2020-02-28 14:45:40 +0530

    Decorators, call, apply, bind

---
+++ AdvancedFunctions/callApplyDecorator.js
@@ -0,0 +1,80 @@
+'use strict';
+function sep() {console.log('--------------------------------------------------')}
+
+
+var fruits = [
+    'Apple',
+    'Orange',
+    'Mango',
+    'Grape',
+    'Watermelon'
+]
+
+function task(){
+    let i = 0
+    while(i < 10000000000)
+        i += 1
+    let index = Math.round(Math.random() * 5)
+    let fruit = fruits[index]
+    return fruit
+}
+
+function enableCache(func){
+    let cache = new Map()
+    return function(name){
+        if(cache.has(name))
+            return cache.get(name)
+        else{
+            let fruit = func()
+            cache.set(name, fruit)
+            return fruit
+        }
+    }
+}
+
+let cachedTask = enableCache(task)
+
+// console.log(cachedTask("vyshnav"))
+// console.log(cachedTask("gokul"))
+// console.log(cachedTask("vyshnav"))
+// console.log(cachedTask("vyshnav"))
+// console.log(cachedTask("gokul"))
+// console.log(cachedTask("vyshnav"))
+
+sep()
+
+// Call function
+
+var person = {
+    name: "Vyshnav",
+    sayHi(){
+        console.log("Hi", this.name)
+    }
+}
+
+person.sayHi() // Hi vyshnav
+
+let func = person.sayHi
+// func() // Error this is undefined
+func.call(person)
+
+sep();
+
+// Apply
+
+var applyObj = {
+    phrase: "Args:",
+    sayHi(...args){
+        console.log(this.phrase, ...args)
+    }
+}
+
+applyObj.sayHi.apply(applyObj, [1,2,3,4,5,6,7,8,9])
+
+sep();
+
+// Bind
+
+let bound = applyObj.sayHi.bind(applyObj, "arg1", "arg2")
+
+bound()
