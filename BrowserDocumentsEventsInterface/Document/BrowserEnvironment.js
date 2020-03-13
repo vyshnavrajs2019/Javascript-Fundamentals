095715ba7b9c142feeae4b2cd22870a5af629349
Vyshnav <svyshnavraj@gmail.com>
2020-03-13 10:25:15 +0530

    Browser Environment

---
+++ BrowserDocumentsEventsInterface/Document/BrowserEnvironment.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// DOM
+console.log(document.body) // body tag
+console.log(document.documentElement) // html tag
+console.log(document.head) // head tag
+
+// Change body background color
+document.body.style.backgroundColor = "red"
+document.body.style.color = "white"
+
+// BOM
+console.log(location.href);
+// Redirect to google.com
+// location.href = "https://www.google.com"
+// Usign window
+// window.location = "https://www.facebook.com"
+// Navigator
+console.log(navigator.userAgent); // gets the browser name
+console.log(navigator.platform); // gets the platform
+
